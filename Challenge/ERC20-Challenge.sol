6f081c7d78615b822013c03d1eec7ecb76dfdba1
Xin Chen <nshen121@gmail.com>
2024-10-13 14:43:52 +0800

    feat: finish challenge 1

--- Challenge/ERC20-Challenge.sol
+++ Challenge/ERC20-Challenge.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Define your ERC20 token contract
 contract MyToken is ERC20 {
@@ -9,35 +9,47 @@ contract MyToken is ERC20 {
 
     // Constructor that mints the initial supply to the deployer of the contract
     constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        // Mint the initial supply of tokens to the deployer's address
+        _mint(msg.sender, initialSupply);
     }
 
     // Function to mint new tokens to a specified address
     function mint(address to, uint256 amount) public {
-        // Implement the mint function using the _mint internal function
+        _mint(to, amount);
     }
 
     // Function to burn tokens from a specified address
     function burn(address from, uint256 amount) public {
-        // Implement the burn function using the _burn internal function
+        _burn(from, amount);
     }
 
     // Function to transfer tokens from the caller's address to a specified address
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        // Implement the transfer function using the _transfer internal function
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
     }
 
     // Function to approve an address to spend a certain amount of tokens on behalf of the caller
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        // Implement the approve function using the _approve internal function
+    function approve(
+        address spender,
+        uint256 amount
+    ) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
     }
 
     // Function to transfer tokens from one address to another using an allowance
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        // Implement the transferFrom function using the _transfer and _spendAllowance internal functions
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        return super.transferFrom(from, to, amount);
     }
 
     function getBalanceOf(address account) public view returns (uint256) {
-        // Implement the getBalanceOf function
+        return balanceOf(account);
     }
 }
